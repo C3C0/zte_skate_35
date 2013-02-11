279b48ce124c811e14acc6e08e1065e2fd884e8d
Grigori Goronzy <greg@blackbox>
2013-02-11 22:40:44 +0100

    si4708: uncrustify
    Apply uncrustify with Linux kernel configuration file, switch to Unix
    line endings.
    
    Change-Id: I31c6df3aa420e1520a5e912166a70653770d6226

--- arch/arm/mach-msm/include/mach/fm_si4708.h
+++ arch/arm/mach-msm/include/mach/fm_si4708.h
@@ -1,6 +1,6 @@
 /*
  *    (C) Copyright  2009, ZTE Corporation
-*/
+ */
 
 #ifndef __SI4708_H__
 #define __SI4708_H__
@@ -8,17 +8,17 @@
 
 
 /******************************  MACROS DEFINE******************************/
-#define      FM_RESET_GPIO    				3
-#define      FM_I2C_SDA_GPIO    		      93
+#define      FM_RESET_GPIO                              3
+#define      FM_I2C_SDA_GPIO                          93
 
-#define      SI4708_RESET_DELAY               110   //ms
-#define      SI4708_POWERUP_DELAY         110   //ms
-#define      SI4708_POWERDOWN_DELAY         110  //ms
-#define      SI4708_SEEK_TUNE_DELAY       888  //ms,change from 60 to 888 for tune test 
+#define      SI4708_RESET_DELAY               110       //ms
+#define      SI4708_POWERUP_DELAY         110           //ms
+#define      SI4708_POWERDOWN_DELAY         110         //ms
+#define      SI4708_SEEK_TUNE_DELAY       888           //ms,change from 60 to 888 for tune test
 /****************************INTERNAL VARIBLE******************************/
 
-#define      SEEKUP  			1		/* seek up*/
-#define      SEEKDOWN 		0		/* seek down*/
+#define      SEEKUP                     1               /* seek up*/
+#define      SEEKDOWN           0                       /* seek down*/
 
 #define      FM_TRACK_STEREO 0
 #define      FM_TRACK_MONO   1
@@ -45,8 +45,8 @@
 #define REG_SEEKUP_MASK 0x02
 #define REG_SKMODE_MASK 0x04
 
-#define FM_VOL_MUTE    0/* volume min.= 0*/
-#define FM_VOL_MAX      15/* volume max.=15*/
+#define FM_VOL_MUTE    0 /* volume min.= 0*/
+#define FM_VOL_MAX      15 /* volume max.=15*/
 
 
 #define FM_REG_STATUSRSSI 0
@@ -69,27 +69,27 @@
 
 /* Interfaces */
 
-#define FM_INIT2NORMAL                 _IO(FM_IOC_MAGIC, 1)/*for test*/
-#define FM_NORMAL2STANDBY         _IO(FM_IOC_MAGIC, 2)/*for test*/
-#define FM_STANDBY2NORMAL         _IO(FM_IOC_MAGIC, 3)/*for test*/
+#define FM_INIT2NORMAL                 _IO(FM_IOC_MAGIC, 1) /*for test*/
+#define FM_NORMAL2STANDBY         _IO(FM_IOC_MAGIC, 2) /*for test*/
+#define FM_STANDBY2NORMAL         _IO(FM_IOC_MAGIC, 3) /*for test*/
 
 #define FM_TUNE                              _IOW(FM_IOC_MAGIC, 4, int)
-#define FM_SEEK        		                _IOW(FM_IOC_MAGIC, 5, int[2])
-//#define FM_AUTOSEEK   		 	   _IOR(FM_IOC_MAGIC, 6)
-#define FM_GET_VOL    		  	   _IOR(FM_IOC_MAGIC, 7, int)
-#define FM_SET_VOL    		          _IOW(FM_IOC_MAGIC, 8,int)
-//#define FM_VOL_UP      		          _IO(FM_IOC_MAGIC, 9)
-//#define FM_VOL_DOWN  		  	    _IO(FM_IOC_MAGIC, 10)
+#define FM_SEEK                                 _IOW(FM_IOC_MAGIC, 5, int[2])
+//#define FM_AUTOSEEK                              _IOR(FM_IOC_MAGIC, 6)
+#define FM_GET_VOL                         _IOR(FM_IOC_MAGIC, 7, int)
+#define FM_SET_VOL                        _IOW(FM_IOC_MAGIC, 8, int)
+//#define FM_VOL_UP                               _IO(FM_IOC_MAGIC, 9)
+//#define FM_VOL_DOWN                       _IO(FM_IOC_MAGIC, 10)
 /* Reserved interface*/
-#define FM_GET_BAND   		  		_IOR(FM_IOC_MAGIC, 11, int)
-#define FM_SET_BAND     	   		       _IOW(FM_IOC_MAGIC, 12, int)
-#define FM_GET_SPACE    	   		       _IOR(FM_IOC_MAGIC, 13, int)
-#define FM_SET_SPACE    	  		       _IOW(FM_IOC_MAGIC, 14, int)
-#define FM_GET_AUDIOTRACK    	       _IOR(FM_IOC_MAGIC, 15, int)
-#define FM_SET_AUDIOTRACK    	       _IOW(FM_IOC_MAGIC, 16, int)
-#define FM_GET_CURRENTFREQ		_IOR(FM_IOC_MAGIC, 17, int)
+#define FM_GET_BAND                             _IOR(FM_IOC_MAGIC, 11, int)
+#define FM_SET_BAND                                    _IOW(FM_IOC_MAGIC, 12, int)
+#define FM_GET_SPACE                                   _IOR(FM_IOC_MAGIC, 13, int)
+#define FM_SET_SPACE                                   _IOW(FM_IOC_MAGIC, 14, int)
+#define FM_GET_AUDIOTRACK              _IOR(FM_IOC_MAGIC, 15, int)
+#define FM_SET_AUDIOTRACK              _IOW(FM_IOC_MAGIC, 16, int)
+#define FM_GET_CURRENTFREQ              _IOR(FM_IOC_MAGIC, 17, int)
 /*for test*/
-#define FM_DISPLAY_REG    			 _IO(FM_IOC_MAGIC, 18)      
+#define FM_DISPLAY_REG                           _IO(FM_IOC_MAGIC, 18)
 
 /******************************************************************************/
 #endif /* __SI4708_H__ */
